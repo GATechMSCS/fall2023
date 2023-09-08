00f7f9d33d023eb4eeb6b5cdc001c8c50c772f11
scottschmidl <sjschmidl@gmail.com>
2023-09-07 19:39:30 -0500

    start bayes hw

---
+++ bayesian_statistics/lectures/week3and4/unit4/homework/code/HW2_Code_ScottSchmidl.py
@@ -0,0 +1,55 @@
+# IMPORTS
+import matplotlib.pyplot as plt
+import networkx as nx
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import CausalInference
+from pgmpy.models import BayesianNetwork
+
+# QUESTION 1
+print("------------QUESTION 1-----------")
+
+# A)
+print("***PART A***")
+
+
+# B)
+print("\n***PART B***")
+
+
+# C)
+print("\n***PART C***")
+
+
+# D)
+print("\n***PART D***")
+
+# QUESTION 2
+print("\n------------QUESTION 2-----------")
+
+
+# A)
+print("***PART A***")
+
+
+# B)
+print("\n***PART B***")
+
+
+# QUESTION 3
+print("\n------------QUESTION 3-----------")
+
+
+# A)
+print("\n***PART A***")
+
+
+# B)
+print("\n***PART B***")
+
+
+# C)
+print("\n***PART C***")
+
+
+# D)
+print("\n***PART D***")
