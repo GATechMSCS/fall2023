bd2b0c72fbf16a0fc0a89aca7f4f90ca9ca2fb14
scottschmidl <sjschmidl@gmail.com>
2023-09-21 21:45:45 -0500

    prepping bs hw 3

---
+++ bayesian_statistics/lectures/week5and6/rest_unt4/homework/code/HW2_Code_ScottSchmidl.py
@@ -0,0 +1,55 @@
+# IMPORTS
+import math as m
+from  scipy.stats import binom
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# QUESTION 1
+print("------------QUESTION 1-----------")
+
+# A)
+print("***PART A***")
+
+
+x = .5
+print(x**(3/16) - -x**(3/16))
+
+# B)
+print("\n***PART B***")
+
+# C)
+print("\n***PART C***")
+
+
+# D)
+print("\n***PART D***")
+
+
+# QUESTION 2
+print("\n------------QUESTION 2-----------")
+'''
+
+'''
+
+# A)
+
+
+# B)
+print("\n***PART B***")
+
+
+# QUESTION 3
+print("\n------------QUESTION 3-----------")
+
+
+# A)
+
+
+# B)
+
+
+# C)
+
+
+# D)
+
