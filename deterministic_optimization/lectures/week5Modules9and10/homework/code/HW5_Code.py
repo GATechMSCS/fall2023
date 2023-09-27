c93fc6979a9c1cd359dbcff852198fa18d2d8294
scottschmidl <sjschmidl@gmail.com>
2023-09-26 21:44:46 -0500

    hw 5 is finished

--- deterministic_optimization/lectures/week5Modules9and10/homework/code/HW5_Code.py
+++ deterministic_optimization/lectures/week5Modules9and10/homework/code/HW5_Code.py
@@ -12,76 +12,77 @@ In .docx
 
 # QUESTION 2
 print("\n------------QUESTION 2-----------")
-x, x_later = np.array([1.2, 1.2]), np.array([-1.2, 1])
+x, x_later = np.matrix([1.2, 1.2]), np.matrix([-1.2, 1])
 
 def f(x):
 
-    return 100 * (x[1] - x[0]**2)**2 + (1 - x[0])**2
+    x1 = x[0,0]
+    x2 = x[0, 1]
+    
+    return 100 * (x2 - x1**2)**2 + (1 - x1)**2
 
 def fprime(x):
-    
-    df_dx2 = (200 * x[1]) - (200*x[1]**2)
-    df_dx1 = (-400 * x[0] * x[1]) + (400 * x[0]**3) - 2 + (2 * x[0])
-    return np.array([df_dx1, df_dx2])
+
+    x1 = x[0,0]
+    x2 = x[0, 1]
+
+    df_dx1 = (-400 * x1 * x2) + (400 * x1**3) - 2 + (2 * x1)    
+    df_dx2 = (200 * x2) - (200*x1**2)
+
+    return np.matrix([[df_dx1], [df_dx2]])
 
 def f_doubleprime(x):
-    dfx1_dx1 = -400 * x[1] + 1200 * x[0]**2 + 2
-    dfx1_dx2 = -400 * x[0]
-    dfx2_dx1 = -400 * x[0] 
-    dfx2_dx2 = 200
 
-    return np.array([[dfx1_dx1, dfx1_dx2],
+    x1 = x[0,0]
+    x2 = x[0, 1]
+    
+    dfx1_dx1 = -400 * x2 + 1200 * x1**2 + 2
+    dfx1_dx2 = -400 * x1
+    dfx2_dx1 = -400 * x1 
+    dfx2_dx2 = 200
+    
+    return np.matrix([[dfx1_dx1, dfx1_dx2],
                     [dfx2_dx1, dfx2_dx2]])
 
 def dk_cal(xk):
 
-    return -1 * np.linalg.inv(f_doubleprime(xk)) @ fprime(xk)
+    return -np.linalg.inv(f_doubleprime(xk)) @ fprime(xk)
 
 def left_right(xk, alphak, dk, c):
-    left_side = f(xk + alphak * dk)
-    right_side = f(xk) + c * alphak * fprime(xk).T @ dk
-
-    print(f'left: {left_side}, right: {right_side}, left > right: {left_side > right_side}')
+    
+    left_side = f(xk + alphak * dk.T)
+    right_side = f(xk) + (c * alphak * fprime(xk)).T @ dk
 
     return left_side > right_side    
 
-# STILL NEEDS TO TEST THIS**************************************
 def newtons_method_line_search(xk):
     
     # set
     k = 0
     eps = m.pow(10, -4)
     dk = dk_cal(xk)
-
     d = {}
     
     while np.linalg.norm(fprime(xk)) > eps:
         # choose alpha, row, and c
-        row = 0.6
-        c = 0.22
-        alphak = 1
+        row = 0.5
+        c = 0.5
+        abar = 1
+        alphak = abar
         n = 1
         while left_right(xk, alphak, dk, c):
-            print(f'alphak (second while): {alphak} n: {n} dk: {dk}\nfprime@dk: {fprime(x)@dk} fprime.T@dk {fprime(x).T@dk}\n')
+            
             alphak = row * alphak
-            n += 1
-        print('xk: ', xk, 'k: ', k, 'alphak (first while): ', alphak)
-        xk = xk + alphak * dk
+            
+        xk = xk + alphak * dk.T
         dk = dk_cal(xk)
         k += 1
-        print(f'after recalculation xk: {xk} k: {k} alphak (first while): {alphak}\ndk: {dk} fprime@dk: {fprime(x)@dk} fprime.T@dk{fprime(x).T@dk}\n')
 
-        d[tuple(xk)] = [k, alphak, xk]
-
-        if k == 2:
-            raise Exception
+        print(f'\n{xk=} {alphak=} {k=}')
         
-    return d
+    return 1
 
-print(f"f(x1, x2): {f(x)}")
-print(f"f'(x1, x2): {fprime(x)}")
-print(f"f''(x1, x2): {f_doubleprime(x)}")
-print(newtons_method_line_search(x))
+#newtons_method_line_search(x)
 
 '''
 first each starting point, print out the step length ak and point xk for every step k.
@@ -97,8 +98,12 @@ This is written in my notebook
 # QUESTION 4
 print("\n------------QUESTION 4-----------")
 
+# COSTS
 # demand
-demand = {'d1': 100, 'd2': 65, 'd3': 95}
+d = np.array([100,  65, 95])
+
+# unit generation costs
+c = np.array([16, 20, 8])
 
 # CONSTRAINTS
 # generator production upper/lower bounds
@@ -106,14 +111,15 @@ p_min = np.array([20, 20, 10])
 p_max = np.array([200, 150, 150])
 
 # flow limits
-fij_min = np.array([-100, -110, -50, -80, -60, -40])
-fij_max = np.array([100, 110, 50, 80, 60, 40])
+f12_min, f12_max = -100, 100
+f23_min, f23_max = -110, 110
+f34_min, f34_max = -50, 50
+f45_min, f45_max = -80, 80
+f56_min, f56_max = -60, 60
+f61_min, f61_max = -40, 40
 
 # branch flow
-branch_flow = np.array([11.6, 5.9, 13.7, 9.8, 5.6, 10.5])
-
-# unit generation costs
-c = np.array([16, 20, 8])
+b = np.array([11.6, 5.9, 13.7, 9.8, 5.6, 10.5])
 
 # PART A
 print("---------------------PART A---------------------")
@@ -129,26 +135,53 @@ f34 = cp.Variable(1)
 f45 = cp.Variable(1)
 f56 = cp.Variable(1)
 f61 = cp.Variable(1)
-#fij = cp.Variable(6)
-p = cp.Variable(3)
+
+p1 = f12 - f61
+p2 = f34 - f23
+p3 = f56 - f45
+
+theta1 = cp.Variable(1)
+theta2 = cp.Variable(1)
+theta3 = cp.Variable(1)
+theta4 = cp.Variable(1)
+theta5 = cp.Variable(1)
+theta6 = cp.Variable(1)
+
+p = cp.hstack([p1, p2, p3])
 
 expr = cp.sum(cp.multiply(c, p))
 
 obj = cp.Minimize(expr)
-constraints =  [p >= p_min, p <= p_max,
-                #fij > fij_min, fij <= fig_max,
-                f12 >= fij_min[0], f12 <= fij_max[0],
-                f23 >= fij_min[1], f23 <= fij_max[1],
-                f34 >= fij_min[2], f34 <= fij_max[2],
-                f45 >= fij_min[3], f45 <= fij_max[3],
-                f56 >= fij_min[4], f56 <= fij_max[4],
-                f61 >= fij_min[5], f61 <= fij_max[5],]
 
-#electric = cp.Problem(obj, constraints)
-#electric.solve()
+constraints = [p >= p_min, 
+               p <= p_max,
+               f12 >= f12_min,
+               f12 <= f12_max,
+               f23 >= f23_min,
+               f23 <= f23_max,
+               f34 >= f34_min,
+               f34 <= f34_max,
+               f45 >= f45_min,
+               f45 <= f45_max,
+               f56 >= f56_min,
+               f56 <= f56_max,
+               f61 >= f61_min,
+               f61 <= f61_max,
+               -d[0] == f23 - f12,
+               -d[1] == f45 - f34,
+               -d[2] == f61 - f56,
+               f12 == b[0] * (theta1 - theta2),
+               f23 == b[0] * (theta2 - theta3),
+               f34 == b[0] * (theta3 - theta4),
+               f45 == b[0] * (theta4 - theta5),
+               f56 == b[0] * (theta5 - theta6),]
 
-# USE THE FINAL VALUES OF FLOW FOR EACH BRANCH HERE
-# theta_difference =  flow_limit / branch_flow
+electric = cp.Problem(obj, constraints)
+electric.solve()
+print("\nThe optimal value is", round(electric.value,2))
+#print(f"{f12.value=}, {f23.value=}, {f34.value=}, {f45.value=}, {f56.value=}, {f61.value=}")
 
 # PART C
 print("---------------------PART C---------------------")
+for i in range(22):
+    print(f"The dual value for the {i} constraint: {constraints[i].dual_value}")
